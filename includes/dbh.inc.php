e06aad506f67bf965f621ef37d3e85291669c995
Thanasis Kouskouras <thanasis.kouskouras@gmail.com>
2025-09-10 03:27:10 +0300

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,13 @@
 <?php
 
 $serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "root";
-$dBName = "questionhouse";
+$dBUsername = "";
+$dBPassword = "";
+$dBName = "";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
+
 }
